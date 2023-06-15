8bb6739fca2c0dccadc1ec195a5a2500b8fd00ad
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-15 20:45:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const a = [1];
 console.dir(a);
 
 const User = function(email, password){
-
+        this.email = email;
+        this.password = password + "%%%";
 };
 
 const user1 = new User('a@a.com', "123");
+const user2 = new User('b@b.com', "123456");
 console.log(user1);
+console.log(user2);
+
+console.log(user1 instanceof User);

1b50fb2719a8bc39538304db7175ade326b3552d
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-15 21:51:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 
+/*
+
 const a = [1];
 console.dir(a);
 
@@ -15,3 +17,24 @@ console.log(user1);
 console.log(user2);
 
 console.log(user1 instanceof User);
+
+*/
+//=========================
+
+const Book = function(title, author){
+    this.author = author;
+    this.title = title;
+    this.isRead = false;
+};
+
+Book.prototype.read = function(){
+    this.isRead = true;
+}
+
+const LOTR = new Book("Lord of the Ring", "Tolkien");
+LOTR.read();
+console.log(LOTR);
+
+console.log(LOTR.__proto__);
+console.log(LOTR.__proto__ === Book.prototype);
+console.log(Book.prototype.isPrototypeOf(LOTR));
